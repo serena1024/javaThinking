4648ec8c210b50c57778da060c8c5bf29610af81
serena1024 <yuyun_serena@163.com>
2019-10-15 20:31:58 +0800

    后台线程Factory

---
+++ src/com/yy/chapter21并发/DaemonFromFactory.java
@@ -0,0 +1,27 @@
+package com.yy.chapter21²¢·¢;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class DaemonFromFactory implements Runnable {
+    @Override
+    public void run() {
+        while(true){
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService exec = Executors.newCachedThreadPool(new DaemonThreadFactory());
+        for (int i = 0 ; i < 10; i++ ){
+            exec.execute(new DaemonFromFactory());
+        }
+        System.out.println("All daemons started   ");
+        TimeUnit.MILLISECONDS.sleep(500);
+    }
+}
