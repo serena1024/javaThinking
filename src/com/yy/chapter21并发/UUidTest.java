5238b3322573438365fc0ac19f8946f3bf242af0
serena1024 <yuyun_serena@163.com>
2019-10-31 16:36:26 +0800

    UUID使用

---
+++ src/com/yy/chapter21并发/UUidTest.java
@@ -0,0 +1,10 @@
+package com.yy.chapter21²¢·¢;
+
+import java.util.UUID;
+
+public class UUidTest {
+    public static void main(String[] args) {
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid);
+    }
+}
