4664ed126a83a465ef5064a4b3910a61242324aa
serena1024 <yuyun_serena@163.com>
2019-10-31 15:18:48 +0800

    自定义类加载器

---
+++ src/com/yy/chapter21并发/MainClassLoader.java
@@ -0,0 +1,45 @@
+package com.yy.chapter21并发;
+
+import java.io.FileInputStream;
+import java.lang.reflect.Method;
+
+public class MainClassLoader {
+    static class MyClassLoader extends ClassLoader {
+        private String classPath;
+
+        public MyClassLoader(String classPath) {
+            this.classPath = classPath;
+        }
+
+        private byte[] loadByte(String name) throws Exception {
+            name = name.replaceAll("\\.", "/");
+            FileInputStream fis = new FileInputStream(classPath + "/" + name
+                    + ".class");
+            int len = fis.available();
+            byte[] data = new byte[len];
+            fis.read(data);
+            fis.close();
+            return data;
+
+        }
+
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            try {
+                byte[] data = loadByte(name);
+                return defineClass(name, data, 0, data.length);
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new ClassNotFoundException();
+            }
+        }
+
+    };
+
+    public static void main(String args[]) throws Exception {
+        MyClassLoader classLoader = new MyClassLoader("D:/ideaForProject/javaThinking");
+        Class clazz = classLoader.findClass("com.yy.chapter21并发.TestClassLoader");
+        Object obj = clazz.newInstance();
+        Method helloMethod = clazz.getDeclaredMethod("hello", null);
+        helloMethod.invoke(obj, null);
+    }
+}
