980f5d0c9e6977a242e2dbdc84b561f13d4421fe
serena <yuyun1024>
2019-11-08 17:29:38 +0800

    适配器模式

---
+++ src/com/yy/interview/Adapter.java
@@ -0,0 +1,17 @@
+package com.yy.interview;
+/***
+ * 适配器模式
+ */
+
+public class Adapter {
+
+
+//    适配器模式的作用就是在原来的类上提供新功能。主要可分为3种：
+//
+//    类适配：创建新类，继承源类，并实现新接口，例如
+//    class  adapter extends oldClass  implements newFunc{}
+//    对象适配：创建新类持源类的实例，并实现新接口，例如
+//    class adapter implements newFunc { private oldClass oldInstance ;}
+//    接口适配：创建新的抽象类实现旧接口方法。例如
+//    abstract class adapter implements oldClassFunc { void newFunc();}
+}
